cb4fb30bc63ae8e65e28c642e6edc4d39a49ec65
Lenovo <sakshishahu81@gmail.com>
2026-08-21 16:06:06 +0530

    commit

--- src/main/java/com/example/AssignmentTracker/AssignmentTrackerApplication.java
+++ src/main/java/com/example/AssignmentTracker/AssignmentTrackerApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AssignmentTrackerApplication {
 
 	public static void main(String[] args) {
+		System.out.println("main");
 		SpringApplication.run(AssignmentTrackerApplication.class, args);
 	}
 
