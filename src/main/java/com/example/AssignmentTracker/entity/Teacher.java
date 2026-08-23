643d94aa28b6982d2ad4f7a584fe3217deccfdc8
nikhil <ny619619@gmail.com>
2026-08-23 23:27:34 +0530

    Erros solved

--- src/main/java/com/example/AssignmentTracker/entity/Teacher.java
+++ src/main/java/com/example/AssignmentTracker/entity/Teacher.java
@@ -4,19 +4,24 @@ import jakarta.persistence.*;
 import lombok.Data;
 import lombok.ToString;
 
-import java.util.List;
-
 @Entity
 @Data
 @ToString
 public class Teacher {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
+
     String name;
+
     @ManyToOne(fetch = FetchType.LAZY)
     Student studentList;
 
     String subject;
 
+    // SuperAdmin who created this teacher
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "super_admin_id")
+    SuperAdmin createdBySuperAdmin;
 }
